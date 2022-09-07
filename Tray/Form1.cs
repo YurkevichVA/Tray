c81c9b2045fe2870abb6d746c3782762bc1af0d2
ovsan <ovsan@NAMENAME>
2022-09-07 18:36:52 +0300

    Add project files.

---
+++ Tray/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tray
+{
+    public partial class Form1 : Form
+    {
+        private int minResizeCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon1.Visible = true;
+                minResizeCount++;
+                notifyIcon1.Text = minResizeCount.ToString();
+                notifyIcon1.BalloonTipText = minResizeCount.ToString();
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if(WindowState == FormWindowState.Minimized)
+            {
+                WindowState = FormWindowState.Normal;
+                notifyIcon1.Visible = false;
+            }
+        }
+    }
+}
